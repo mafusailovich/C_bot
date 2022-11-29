acfba19ab79693bd0a717184da5a59e035f756ff
mafusailovich <118286637+mafusailovich@users.noreply.github.com>
2022-11-29 09:28:15 +0300

    Update extensions.py
    Исправил ошибку в названии APIException (было APIExceprion, стало APIException).

--- extensions.py
+++ extensions.py
@@ -2,7 +2,7 @@ import requests
 import json
 from config import keys
 
-class APIExceprion(Exception):
+class APIException(Exception):
     pass
 
 class Converter:
@@ -10,22 +10,22 @@ class Converter:
     @staticmethod
     def get_price(quote:str, base: str, amount:str):
         if quote == base:
-            raise APIExceprion(f'Невозможно перевести одинаковые валюты в {base}')
+            raise APIException(f'Невозможно перевести одинаковые валюты в {base}')
 
         try:
             quote_ticker = keys[quote]
         except KeyError:
-            raise APIExceprion(f'Не удалось обработать валюту {quote}')
+            raise APIException(f'Не удалось обработать валюту {quote}')
 
         try:
             base_ticker = keys[base]
         except KeyError:
-            raise APIExceprion(f'Не удалось обработать валюту {base}')
+            raise APIException(f'Не удалось обработать валюту {base}')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise APIExceprion(f'Не удалось обработать количество {amount}')
+            raise APIException(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://currate.ru/api/?get=rates&pairs={quote_ticker}{base_ticker}&key=12c53cc5c30d7dbfbe27fcea48fac5a2')
         total_base = json.loads(r.content)['data'][f'{quote_ticker}{base_ticker}']
